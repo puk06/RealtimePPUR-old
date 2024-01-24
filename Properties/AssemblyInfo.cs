7c86195d12b8adffb8654c2b476202eaf86f0499
Yuki Naka <86549420+puk06@users.noreply.github.com>
2024-01-25 01:13:28 +0900

    v1.2.7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RealtimePPUR")]
-[assembly: AssemblyCopyright("Copyright © 2023 Hoshino1")]
+[assembly: AssemblyCopyright("Copyright © 2024 Hoshino1")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.6.0")]
-[assembly: AssemblyFileVersion("1.2.6.0")]
+[assembly: AssemblyVersion("1.2.7.0")]
+[assembly: AssemblyFileVersion("1.2.7.0")]
 [assembly: NeutralResourcesLanguage("en")]
